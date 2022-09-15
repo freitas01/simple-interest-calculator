acfab1c6ba3d296e9968835383b87760ee21e81a
freitas01 <96348299+freitas01@users.noreply.github.com>
2022-09-15 18:47:01 -0300

    FinalExam
    ok

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function compute(){
- 
+
     p = document.getElementById("principal").value;
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
@@ -8,9 +8,19 @@ function compute(){
     var year = new Date().getFullYear()+parseInt(years);
     var amount = parseInt(interest);
     
-     /*document.getElementById("result").innerText=interest;*/
-    document.getElementById("result").innerHTML = interest + "<br><br>" + "If you deposit " + principal + ",\<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of " + amount + ",\<br\>in the year " + year + "\<br\>";
+         /*document.getElementById("result").innerText=interest;*/
+   // document.getElementById("result").innerHTML = interest + "<br><br>" + "If you deposit " + principal + ",\<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of " + amount + ",\<br\>in the year " + year + "\<br\>";
+   
+   if (principal <= 0 ) {
+   alert('Enter a positive number')
+   return document.getElementById("principal").focus();
+   
+   }
+   else {
+    document.getElementById("result").innerHTML="If you deposit "+"<span style='background:yellow;'>"+principal+"</span>"+",\<br\>at an interest rate of "+"<span style='background:yellow;'>"+rate+"</span>"+"<span style='background:yellow;'>%</span>\<br\>You will receive an amount of "+"<span style='background:yellow;'>"+amount+"</span>"+",\<br\>in the year "+"<span style='background:yellow;'>"+year+"</span>"+"\<br\>"
+   }
 
+    
     /*console.log(principal);
     console.log(interest);
     console.log(principal+interest);*/
